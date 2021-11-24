9bd0c2c948e237c3ef3fc6d384ac2840f5b446be
Jhonata Augusto <jhon.augustosilva@gmail.com>
2021-11-24 10:39:47 -0300

    Implementação da API dio
    Troca do provedor da lista de objetos json para uma API que faz o get dessa lista a partir do jsonPlaceHolder

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:projeto01/controllers/home_controller.dart';
 import 'package:projeto01/models/post_model.dart';
-import 'package:projeto01/repositories/home_repository_mock.dart';
+import 'package:projeto01/repositories/home_repository_imp.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,7 +11,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final HomeController _controller = HomeController(HomeRepositoryMock());
+  final HomeController _controller = HomeController(HomeRepositoryImp());
 
   @override
   void initState() {
@@ -22,14 +22,23 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
         body: ValueListenableBuilder<List<PostModel>>(
-            valueListenable: _controller.posts,
-            builder: (_, list, __) {
-              return ListView.builder(
-                  itemCount: list.length,
-                  itemBuilder: (_, idx) => ListTile(
-                        title: Text(list[idx].title),
-                      ));
-            }));
+          valueListenable: _controller.posts,
+          builder: (_, list, __) {
+            return ListView.separated(
+              shrinkWrap: true,
+              itemCount: list.length,
+              itemBuilder: (_, idx) => ListTile(
+                leading: Text(list[idx].id.toString()),
+                title: Text(list[idx].title),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+              separatorBuilder: (_, __) => Divider(),
+            );
+          },
+        ));
   }
 }
